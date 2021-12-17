014da6574288a9d3722063f595a3528fcd91d286
alexl <alexlam1994@live.com>
2021-12-17 23:27:57 +1100

    updated staff_schema

--- my_app/schemas/staff_schema.py
+++ my_app/schemas/staff_schema.py
@@ -2,11 +2,10 @@ from main import ma
 from models.staffs import Staff
 from marshmallow_sqlalchemy import auto_field
 from marshmallow.validate import Length, Email
-from schemas.pharmacy_schema import PharmacySchema
 
 class StaffSchema(ma.SQLAlchemyAutoSchema):
     staff_id = auto_field(dump_only=True)
-    staff_name = auto_field(required=True, validate=Length(min=1))
+    staff_name = auto_field(required=True, validate=Length(min=1, max=50))
     staff_email = auto_field(required=True, validate = Email())
     staff_dob = auto_field(required=False, validate=Length(min=1))
     creator = ma.Nested("PharmacySchema")
